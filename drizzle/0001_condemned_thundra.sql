4780d6e70d15f8b31f6b47d3775a8d2ebf1d5c3b
tol-prog <tolerashula@gmail.com>
2026-09-13 09:39:37 +0300

    Create 0001_condemned_thundra.sql

---
+++ drizzle/0001_condemned_thundra.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "livestock_activity" ADD COLUMN "regular_eggs" integer;--> statement-breakpoint
+ALTER TABLE "livestock_activity" ADD COLUMN "oversized_eggs" integer;--> statement-breakpoint
+ALTER TABLE "livestock_activity" ADD COLUMN "broken_eggs" integer;
